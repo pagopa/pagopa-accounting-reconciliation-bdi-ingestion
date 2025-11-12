e2aadae2cf258ce6822f032e49373a0772338ef6
ivan.ferrando <ivan.ferrando@pagopa.it>
2025-11-12 14:38:53 +0100

    feat(setup): add first setup (spring initializr)

---
+++ src/main/kotlin/it/pagopa/accounting/reconciliation/bdi/ingestion/Application.kt
@@ -0,0 +1,11 @@
+package it.pagopa.accounting.reconciliation.bdi.ingestion
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
---
+++ src/test/kotlin/it/pagopa/accounting/reconciliation/bdi/ingestion/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package it.pagopa.accounting.reconciliation.bdi.ingestion
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

35f6224428f7d1a9808f97e88819a0a86d5ff86c
ivan.ferrando <ivan.ferrando@pagopa.it>
2025-11-12 17:40:53 +0100

    feat(setup): enable scheduling

--- src/main/kotlin/it/pagopa/accounting/reconciliation/bdi/ingestion/PagopaAccountingReconciliationBdiIngestionApplication.kt
+++ src/main/kotlin/it/pagopa/accounting/reconciliation/bdi/ingestion/PagopaAccountingReconciliationBdiIngestionApplication.kt
@@ -2,8 +2,10 @@ package it.pagopa.accounting.reconciliation.bdi.ingestion
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class PagopaAccountingReconciliationBdiIngestionApplication
 
 fun main(args: Array<String>) {

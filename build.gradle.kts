de612f74bf0372862d6f1cdbb4fe40c7e9828fb0
ivan.ferrando <ivan.ferrando@pagopa.it>
2025-11-12 16:53:29 +0100

    feat(setup): update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,13 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
+	kotlin("jvm") version "2.1.0"
+	kotlin("plugin.spring") version "2.1.0"
 	id("org.springframework.boot") version "3.5.7"
 	id("io.spring.dependency-management") version "1.1.7"
+    id("com.diffplug.spotless") version "8.0.0"
+    id("org.sonarqube") version "7.0.1.6134"
+    id("com.dipien.semantic-version") version "2.0.0" apply false
+    jacoco
+    application
 }
 
 group = "it.pagopa.accounting.reconciliation"
@@ -19,6 +24,23 @@ repositories {
 	mavenCentral()
 }
 
+object Dependencies {
+    const val ecsLoggingVersion = "1.5.0"
+    const val openTelemetryVersion = "1.37.0"
+    const val mockitoVersion = "6.1.0"
+}
+
+dependencyLocking {
+    lockAllConfigurations()
+}
+
+dependencyManagement {
+    imports { mavenBom("org.springframework.boot:spring-boot-dependencies:3.5.7") }
+    // Kotlin BOM
+    imports { mavenBom("org.jetbrains.kotlin:kotlin-bom:2.2.21") }
+    imports { mavenBom("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.10.2") }
+}
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
@@ -26,11 +48,25 @@ dependencies {
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("co.elastic.logging:logback-ecs-encoder:${Dependencies.ecsLoggingVersion}")
+    implementation("io.opentelemetry:opentelemetry-api:${Dependencies.openTelemetryVersion}")
+
+    // tests
+    testImplementation("org.mockito.kotlin:mockito-kotlin:${Dependencies.mockitoVersion}")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+configurations {
+    implementation.configure {
+        exclude(module = "spring-boot-starter-web")
+        exclude("org.apache.tomcat")
+        exclude(group = "org.slf4j", module = "slf4j-simple")
+    }
 }
 
 kotlin {
@@ -39,6 +75,78 @@ kotlin {
 	}
 }
 
+springBoot {
+    mainClass.set("it.pagopa.accounting.reconciliation.bdi.ingestion.PagopaAccountingReconciliationBdiIngestionApplicationKt")
+    buildInfo {
+        properties {
+            additional.set(mapOf("description" to (project.description ?: "Default description")))
+        }
+    }
+}
+
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.named<Jar>("jar") {
+    enabled = false
+}
+
+tasks.register("applySemanticVersionPlugin") {
+    group = "semantic-versioning"
+    description = "Semantic versioning plugin"
+    dependsOn("prepareKotlinBuildScriptModel")
+    apply(plugin = "com.dipien.semantic-version")
+}
+
+configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+    kotlin {
+        toggleOffOn()
+        targetExclude("build/**/*")
+        ktfmt().kotlinlangStyle()
+    }
+    kotlinGradle {
+        toggleOffOn()
+        targetExclude("build/**/*.kts")
+        ktfmt().googleStyle()
+    }
+    java {
+        target("**/*.java")
+        targetExclude("build/**/*")
+        eclipse().configFile("eclipse-style.xml")
+        toggleOffOn()
+        removeUnusedImports()
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+
+    classDirectories.setFrom(
+        files(
+            classDirectories.files.map {
+                fileTree(it).matching {
+                    exclude("it/pagopa/accounting/reconciliation/bdi/ingestion/PagopaAccountingReconciliationBdiIngestionApplicationKt.class")
+                }
+            }
+        )
+    )
+
+    reports { xml.required.set(true) }
+}
+
+/**
+ * Task used to expand application properties with build specific properties such as artifact name
+ * and version
+ */
+tasks.processResources {
+    filesMatching("application.properties") {
+        expand(project.properties)
+    }
 }

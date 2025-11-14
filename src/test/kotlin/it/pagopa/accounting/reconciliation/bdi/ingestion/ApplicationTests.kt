1c039eda72232d3ab5dd6fa7750629ced01ce859
Simone Infante <52280205+infantesimone@users.noreply.github.com>
2025-11-14 14:36:19 +0100

    fix: application.properties for testing

--- src/test/kotlin/it/pagopa/accounting/reconciliation/bdi/ingestion/ApplicationTests.kt
+++ src/test/kotlin/it/pagopa/accounting/reconciliation/bdi/ingestion/ApplicationTests.kt
@@ -2,9 +2,12 @@ package it.pagopa.accounting.reconciliation.bdi.ingestion
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
 
 @SpringBootTest
+@TestPropertySource(locations = ["classpath:application.test.properties"])
 class ApplicationTests {
 
     @Test fun contextLoads() {}
 }
+

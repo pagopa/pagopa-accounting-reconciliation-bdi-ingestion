f93266a7a2408c488dd3102b737df4cec92a5251
sterondinella <stefano.rondinella@pagopa.it>
2025-11-26 14:36:06 +0100

    refactoring(webclientsconfig): apply spotless

--- src/main/kotlin/it/pagopa/accounting/reconciliation/bdi/ingestion/config/WebClientsConfig.kt
+++ src/main/kotlin/it/pagopa/accounting/reconciliation/bdi/ingestion/config/WebClientsConfig.kt
@@ -32,7 +32,9 @@ class WebClientsConfig {
 
         // Configure the SSLcontext of Netty using the KeyManager
         val sslContext =
-            SslContextBuilder.forClient().keyManager(sslBundle.managers.keyManagerFactory).build()
+            SslContextBuilder.forClient()
+                .keyManager(sslBundle.managers.keyManagerFactory)
+                .build()
 
         val httpClient =
             HttpClient.create()

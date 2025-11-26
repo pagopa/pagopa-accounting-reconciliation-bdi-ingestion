ba3bfce1b1299480ac7fbe9747d88e04f39d3654
sterondinella <stefano.rondinella@pagopa.it>
2025-11-26 11:04:47 +0100

    feat(webclientconfig): add ssl keystore configuration with spring bundle

--- src/main/kotlin/it/pagopa/accounting/reconciliation/bdi/ingestion/config/WebClientsConfig.kt
+++ src/main/kotlin/it/pagopa/accounting/reconciliation/bdi/ingestion/config/WebClientsConfig.kt
@@ -1,11 +1,13 @@
 package it.pagopa.accounting.reconciliation.bdi.ingestion.config
 
 import io.netty.channel.ChannelOption
+import io.netty.handler.ssl.SslContextBuilder
 import io.netty.handler.timeout.ReadTimeoutHandler
 import it.pagopa.generated.bdi.ApiClient as BdiApiClient
 import it.pagopa.generated.bdi.api.AccountingApi
 import java.util.concurrent.TimeUnit
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.ssl.SslBundles
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
@@ -22,8 +24,15 @@ class WebClientsConfig {
         @Value("\${bdi.server.uri}") serverUri: String,
         @Value("\${bdi.server.readTimeoutMillis}") readTimeoutMillis: Int,
         @Value("\${bdi.server.connectionTimeoutMillis}") connectionTimeoutMillis: Int,
+        sslBundles: SslBundles,
     ): WebClient {
-        // TODO: add certificate configuration
+        // Create the bundle spring from the name defined into the application properties for the
+        // bdi service
+        val sslBundle = sslBundles.getBundle("bdi-service")
+
+        // Configure the SSLcontext of Netty using the KeyManager
+        val sslContext =
+            SslContextBuilder.forClient().keyManager(sslBundle.managers.keyManagerFactory).build()
 
         val httpClient =
             HttpClient.create()
@@ -33,6 +42,8 @@ class WebClientsConfig {
                         ReadTimeoutHandler(readTimeoutMillis.toLong(), TimeUnit.MILLISECONDS)
                     )
                 }
+                // SSL configuration
+                .secure { t -> t.sslContext(sslContext) }
                 .resolver { nameResolverSpec: NameResolverProvider.NameResolverSpec ->
                     nameResolverSpec.ndots(1)
                 }

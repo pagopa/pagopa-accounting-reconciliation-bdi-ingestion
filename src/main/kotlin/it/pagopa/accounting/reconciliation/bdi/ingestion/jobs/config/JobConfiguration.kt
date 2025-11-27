adea8cd5553347cc22cb048df50e20e442b9b949
ivan.ferrando <ivan.ferrando@pagopa.it>
2025-11-27 14:25:39 +0100

    feat(data-ingestion): add ScheduledJob interface and JobConfiguration

---
+++ src/main/kotlin/it/pagopa/accounting/reconciliation/bdi/ingestion/jobs/config/JobConfiguration.kt
@@ -0,0 +1,7 @@
+package it.pagopa.accounting.reconciliation.bdi.ingestion.jobs.config
+
+/**
+ * Job configuration common parameter class: every job have its own configuration class that will
+ * extend
+ */
+sealed class JobConfiguration

7a4e2e17d5e589c54c7b314a0d04518bd7df6ab5
ivan.ferrando <ivan.ferrando@pagopa.it>
2025-12-01 09:55:19 +0100

    chore(data-explorer): remove unused code + spotless

--- src/main/kotlin/it/pagopa/accounting/reconciliation/bdi/ingestion/config/KustoConfig.kt
+++ src/main/kotlin/it/pagopa/accounting/reconciliation/bdi/ingestion/config/KustoConfig.kt
@@ -1,11 +1,7 @@
 package it.pagopa.accounting.reconciliation.bdi.ingestion.config
 
 import com.azure.identity.DefaultAzureCredentialBuilder
-import com.microsoft.azure.kusto.data.Client
-import com.microsoft.azure.kusto.data.ClientFactory
 import com.microsoft.azure.kusto.data.auth.ConnectionStringBuilder
-import com.microsoft.azure.kusto.data.auth.endpoints.KustoTrustedEndpoints
-import com.microsoft.azure.kusto.data.auth.endpoints.MatchRule
 import com.microsoft.azure.kusto.ingest.IngestClientFactory
 import com.microsoft.azure.kusto.ingest.QueuedIngestClient
 import org.slf4j.LoggerFactory
@@ -16,7 +12,6 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class KustoConfig {
     private val logger = LoggerFactory.getLogger(javaClass)
-
 
     @Bean(name = ["reKustoClient"])
     fun reKustoClient(
@@ -29,22 +24,6 @@ class KustoConfig {
         logger.info("Initializing RE Kusto client")
 
         val ingestionEndpoint = "https://ingest-$endpoint"
-        //        val csb: ConnectionStringBuilder? =
-        //            ConnectionStringBuilder.createWithAadApplicationCredentials(
-        //                ingestionEndpoint,
-        //                clientId,
-        //                applicationKey,
-        //                tenantId,
-        //            )
-        //        val csb: ConnectionStringBuilder? =
-        //            ConnectionStringBuilder.createWithAadManagedIdentity(
-        //
-        // "https://dataexplorer.azure.com/?cluster=ingest-pagopaddataexplorer.westeurope&workspace=empty",
-        //                "cf9b8103-75d1-4275-ae93-24a2c745625d",
-        //            )
-        //        return IngestClientFactory.createClient(csb)
-
-        val clusterUri = ingestionEndpoint
 
         // 1. Create the DefaultAzureCredential.
         // In an AKS pod with Workload Identity enabled, this automatically
@@ -52,7 +31,7 @@ class KustoConfig {
         val credential = DefaultAzureCredentialBuilder().build()
 
         // 2. Configure the Kusto Connection String to use the TokenCredential
-        val kcsb = ConnectionStringBuilder.createWithTokenCredential(clusterUri, credential)
+        val kcsb = ConnectionStringBuilder.createWithTokenCredential(ingestionEndpoint, credential)
 
         // 3. Create the Ingest Client (Queued Ingestion is recommended for high throughput)
         return IngestClientFactory.createClient(kcsb)

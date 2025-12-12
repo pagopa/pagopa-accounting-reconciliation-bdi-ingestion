981dc1dd44ed910350e54d4cbafd3e07e36c6c52
ivan.ferrando <ivan.ferrando@pagopa.it>
2025-12-12 11:58:46 +0100

    feat(mongoDB): add @CreatedDate and @LastModifiedDate to zip and xml documents

--- src/main/kotlin/it/pagopa/accounting/reconciliation/bdi/ingestion/documents/AccountingZipDocument.kt
+++ src/main/kotlin/it/pagopa/accounting/reconciliation/bdi/ingestion/documents/AccountingZipDocument.kt
@@ -1,16 +1,18 @@
 package it.pagopa.accounting.reconciliation.bdi.ingestion.documents
 
 import java.time.Instant
-import java.util.UUID
+import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.mongodb.core.mapping.Field
 import org.springframework.data.mongodb.core.mapping.FieldType
 
 @Document("accounting-zip")
 data class AccountingZipDocument(
-    @Id val id: String = UUID.randomUUID().toString(),
+    @Id val id: String? = null,
     val filename: String,
-    val uploadedAt: Instant = Instant.now(),
+    @CreatedDate val createdAt: Instant? = null,
+    @LastModifiedDate val updatedAt: Instant? = null,
     @Field(targetType = FieldType.STRING) val status: AccountingZipStatus,
 )

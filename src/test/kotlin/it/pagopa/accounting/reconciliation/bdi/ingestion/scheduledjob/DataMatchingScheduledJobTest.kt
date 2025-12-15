e83be8d1127730bdcd38f8046b16592c8932fbaa
Stefano Rondinella <stefrondinella@live.it>
2025-12-15 17:07:02 +0100

    feat(DataMatchingScheduledJob): add tests for DataMatchingScheduledJob

---
+++ src/test/kotlin/it/pagopa/accounting/reconciliation/bdi/ingestion/scheduledjob/DataMatchingScheduledJobTest.kt
@@ -0,0 +1,77 @@
+package it.pagopa.accounting.reconciliation.bdi.ingestion.scheduledjob
+
+import com.microsoft.azure.kusto.data.Client
+import com.microsoft.azure.kusto.data.KustoOperationResult
+import it.pagopa.accounting.reconciliation.bdi.ingestion.exceptions.MatchingJobException
+import java.time.Duration
+import kotlin.test.Test
+import kotlin.test.assertTrue
+import org.mockito.Mockito.mock
+import org.mockito.kotlin.any
+import org.mockito.kotlin.given
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import reactor.core.publisher.Mono
+import reactor.test.StepVerifier
+
+class DataMatchingScheduledJobTest {
+
+    private val kustoClient: Client = mock()
+    private val timeout: Long = 10
+    private val database: String = "test"
+    private val bdiTimeshift: Int = 7
+    private val fdiTimeshift: Int = 7
+    private val bdiTable: String = "bdiTable"
+    private val fdiTable: String = "fdiTable"
+    private val matchingTable: String = "matchingTable"
+    private val regexCausaleQuery: String = "regexCausaleQuery"
+    private val retries: Long = 2
+    private val minBackoffSeconds: Long = 15
+
+    private val dataMatchingScheduledJob =
+        DataMatchingScheduledJob(
+            kustoClient,
+            timeout,
+            database,
+            bdiTimeshift,
+            fdiTimeshift,
+            bdiTable,
+            fdiTable,
+            matchingTable,
+            regexCausaleQuery,
+            retries,
+            minBackoffSeconds,
+        )
+
+    @Test
+    fun `should create the new table using the matching query`() {
+
+        var responseObj: KustoOperationResult = mock()
+
+        given(kustoClient.executeQueryAsync(any(), any(), any())).willReturn(Mono.just(responseObj))
+
+        StepVerifier.create(dataMatchingScheduledJob.matchingQuery())
+            .expectSubscription()
+            .verifyComplete()
+
+        // Verify that the kusto client is called only 1 time
+        verify(kustoClient, times(1)).executeQueryAsync(any(), any(), any())
+    }
+
+    @Test
+    fun `should fail the to call the client for execute the query, must retry and then call throw an Error`() {
+
+        val exception = Exception()
+
+        given(kustoClient.executeQueryAsync(any(), any(), any())).willReturn(Mono.error(exception))
+
+        StepVerifier.create(dataMatchingScheduledJob.matchingQuery())
+            .expectSubscription()
+            .thenAwait(Duration.ofSeconds(30))
+            .expectErrorSatisfies { error ->
+                assertTrue { error.javaClass == MatchingJobException::class.java }
+                assertTrue { error.cause == exception }
+            }
+            .verify()
+    }
+}

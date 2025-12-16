486b26d3f058e757d6727a679e039eb61e545c8b
ivan.ferrando <ivan.ferrando@pagopa.it>
2025-12-16 12:33:01 +0100

    chore(data-ingestion): refactor IngestionService

--- src/main/kotlin/it/pagopa/accounting/reconciliation/bdi/ingestion/service/ReactiveP7mZipService.kt
+++ src/main/kotlin/it/pagopa/accounting/reconciliation/bdi/ingestion/service/ReactiveP7mZipService.kt
@@ -56,7 +56,7 @@ class ReactiveP7mZipService(
             }
             .buffer(10)
             .zipWith(Flux.interval(Duration.ofMillis(1000)).onBackpressureDrop())
-            .flatMap { tuple ->
+            .concatMap { tuple ->
                 xmlRepository
                     .saveAll(tuple.t1)
                     .flatMap(
